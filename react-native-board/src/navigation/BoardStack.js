f07365412e29420ea332ef1e98e396af5faa7dc2
ChoHanEarl <hanearl3754@naver.com>
2024-11-25 15:43:33 +0900

    게시판 만들기

---
+++ react-native-board/src/navigation/BoardStack.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import MainScreen from "../screens/MainScreen";
+import WriteScreen from "../screens/WriteScreen";
+import { Pressable, View, Text, StyleSheet } from "react-native";
+import {AntDesign} from '@expo/vector-icons'
+
+const Stack = createStackNavigator();
+
+const BoardStack = ({navigation}) => {
+     
+    return(
+        <Stack.Navigator
+            screenOptions={{
+                headerStyle: {
+                    backgroundColor: '#1e1e1e',
+                },
+                headerTitleStyle:{
+                    color:'white',
+                },
+                headerTitleAlign:'center',
+            }}>
+            <Stack.Screen 
+                name="Main" 
+                component={MainScreen}
+                options={{
+                    title: '게시글 목록',
+                }}
+            />
+            <Stack.Screen 
+                name="Write" 
+                component={WriteScreen}
+                options={({navigation}) => ({
+                    title:'글쓰기',
+                    headerLeft:() => (
+                        <Pressable onPress={()=> navigation.goBack()} style={{marginLeft:20}}>
+                            <AntDesign name="close" size={24} color="white"/>
+                        </Pressable>
+                    )
+                })}
+            />
+        </Stack.Navigator>
+    )
+}
+
+export default BoardStack;
